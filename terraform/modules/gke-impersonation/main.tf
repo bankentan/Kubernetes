3109f2a47ce865771652f9a2a49543c95f47c51d
bankentan <bankentan@gmail.com>
2020-09-13 18:43:58 +0800

    test

--- terraform/modules/gke-impersonation/main.tf
+++ terraform/modules/gke-impersonation/main.tf
@@ -1,6 +1,6 @@
 resource "google_service_account_iam_binding" "sa_impersonation" {
   count = "${length(var.iam_policy)}"
-  service_account_id = "projects/${gcp_project}/serviceAccounts/${var.iam_policy[count.index][0]}@${gcp_project}.iam.gserviceaccount.com"
+  service_account_id = "projects/${var.gcp_project}/serviceAccounts/${var.iam_policy[count.index][0]}@${var.gcp_project}.iam.gserviceaccount.com"
   role = "roles/${var.iam_policy[count.index][2]}"
   members = var.iam_policy[count.index][1]
 }
